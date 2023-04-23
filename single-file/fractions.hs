dcf925ab8d57ddc24c096a2215c0f63fd29414cc
Rellikeht <michaelhemperek@gmail.com>
2023-04-23 14:29:38 +0200

    fractions

--- single-file/fractions.hs
+++ single-file/fractions.hs
@@ -3,19 +3,6 @@ module Fractions(Fraction(Frac)) where
 import Data.Ratio
 
 -------------------------------------------------------------------------------
--- Helpers
--------------------------------------------------------------------------------
-
-mygcd a b
- | b == 0 = a
- | otherwise = mygcd b (mod a b)
-
-mylcm a b = quot (fromIntegral m) g
- where
-  m = fromInteger a * fromInteger b
-  g = mygcd a b
-
--------------------------------------------------------------------------------
 -- Definition and instantiation
 -------------------------------------------------------------------------------
 
@@ -30,38 +17,65 @@ mylcm a b = quot (fromIntegral m) g
 
 data Fraction = Frac {num, den :: Int}
 
+-------------------------------------------------------------------------------
+-- Representation
+-------------------------------------------------------------------------------
+
 instance Show Fraction where
- show f = showF f
+ show (Frac n d) = show n ++ "/" ++ show d
 
 instance Read Fraction where
- readsPrec _ f = readsF f
-
-instance Eq Fraction where
- (==) f1 f2 = eq f1 f2
-
-instance Num Fraction where
- (+) f1 f2 = addF f1 f2
- (-) f1 f2 = subF f1 f2
- (*) f1 f2 = mulF f1 f2
- abs f = absF f
- signum f = sigF f
- fromInteger n = Frac (fromInteger n) 1
-
-instance Fractional Fraction where
- (/) f1 f2 = divF f1 f2
- recip f = recipF f
- fromRational n = conv n
-
-instance Ord Fraction where
- (<=) f1 f2 = leF f1 f2
- (<) f1 f2 = lF f1 f2
+ readsPrec _ s = [(short $ Frac dn dd, leftover)]
+  where
+   dn = read (takeWhile isnum s)
+   butfirst = dropWhile isnum s
+   rest = dropWhile nisnum butfirst
+   dd = read (takeWhile isnum rest)
+   leftover = dropWhile isnum rest
+   isnum ch = (code >= 48) && (code <= 57)
+    where code = fromEnum ch
+   nisnum ch = not $ isnum ch
 
 instance Real Fraction where
- toRational f = ratF f
+ toRational (Frac n d) = fromIntegral n % fromIntegral d
 
--------------------------------------------------------------------------------
--- Operations on one fraction
--------------------------------------------------------------------------------
+instance Eq Fraction where
+ (==) f1 f2 = eqs (short f1) (short f2)
+
+instance Num Fraction where
+ (+) (Frac n1 d1) (Frac n2 d2) = short $ Frac n d
+  where
+   g = mygcd d1 d2
+   d = d1*d2
+   n = n1*d2 + n2*d1
+ (-) s (Frac n d) = s+Frac (-n) d
+ (*) f1 f2 = short $ Frac n d
+  where
+   n = num f1 * num f2
+   d = den f1 * den f2
+ abs (Frac n d) = Frac (abs n) (abs d)
+ signum (Frac n d) = Frac (signum n) 1
+ fromInteger n = Frac (fromInteger n) 1
+
+powF (Frac n d) i = Frac (n^i) (d^i)
+
+instance Fractional Fraction where
+ (/) f1 f2 = f1 * recip f2
+ recip (Frac n d) = short $ Frac d n
+ fromRational n = short $ Frac i p
+  where
+   v = fromRational n
+   l = round (logBase 10 v)
+   p = 10^l
+   i = round $ fromIntegral p * v
+
+
+instance Ord Fraction where
+ (<=) f1 f2 = (f1 == f2) || f1 < f2
+ (<) (Frac n1 d1) (Frac n2 d2) = c1 < c2
+  where
+   c1 = fromIntegral n1 * fromIntegral d2
+   c2 = fromIntegral n2 * fromIntegral d1
 
 short f
  | g == 1 = f
@@ -71,32 +85,19 @@ short f
    d = den f
    g = mygcd n d
 
-recipF (Frac n d) = short $ Frac d n
-
-absF (Frac n d) = Frac (abs n) (abs d)
-sigF (Frac n d) = Frac (signum n) 1
-
 -------------------------------------------------------------------------------
--- Operations on two fractions
+-- Helpers
 -------------------------------------------------------------------------------
 
-addF (Frac n1 d1) (Frac n2 d2) = short $ Frac n d
+mygcd a b
+ | b == 0 = a
+ | otherwise = mygcd b (mod a b)
+
+mylcm a b = quot (fromIntegral m) g
  where
-  g = mygcd d1 d2
-  d = d1*d2
-  n = n1*d2 + n2*d1
+  m = fromInteger a * fromInteger b
+  g = mygcd a b
 
-subF s (Frac n d) = addF s (Frac (-n) d)
-
-mulF f1 f2 = short $ Frac n d
- where
-  n = num f1 * num f2
-  d = den f1 * den f2
-
-divF f1 f2 = mulF f1 (recipF f2)
-powF (Frac n d) i = Frac (n^i) (d^i)
-
-eq f1 f2 = eqs (short f1) (short f2)
 
 eqs f1 f2 = (n1 == n2) && (d1 == d2)
  where
@@ -104,37 +105,3 @@ eqs f1 f2 = (n1 == n2) && (d1 == d2)
   n2 = num f2
   d1 = den f1
   d2 = den f2
-
-lF (Frac n1 d1) (Frac n2 d2) = c1 < c2
- where
-  c1 = fromIntegral n1 * fromIntegral d2
-  c2 = fromIntegral n2 * fromIntegral d1
-
-leF f1 f2 = (f1 == f2) || (lF f1 f2)
-
--------------------------------------------------------------------------------
--- Representation
--------------------------------------------------------------------------------
-
-showF (Frac n d) = show n ++ "/" ++ show d
-
-readsF s = [(short $ Frac dn dd, leftover)]
- where
-  dn = read (takeWhile isnum s)
-  butfirst = dropWhile isnum s
-  rest = dropWhile nisnum butfirst
-  dd = read (takeWhile isnum rest)
-  leftover = dropWhile isnum rest
-  isnum ch = (code >= 48) && (code <= 57)
-   where code = fromEnum ch
-  nisnum ch = not $ isnum ch
-
-conv n = short $ Frac i p
- where
-  v = fromRational n
-  l = round (logBase 10 v)
-  p = 10^l
-  i = round $ fromIntegral p * v
-
-ratF :: Fraction -> Rational
-ratF (Frac n d) = fromIntegral n % fromIntegral d
